eada5a56cb857b04a66912e19cf21828531464fd
Gavin M. Roy <gavinmroy@gmail.com>
2014-10-09 00:39:28 -0400

    Add an include with records

---
+++ include/pg_graph_dump.hrl
@@ -0,0 +1,27 @@
+%%=============================================================================
+%% @author Gavin M. Roy <gavinr@aweber.com>
+%% @copyright 2014 AWeber Communications
+%% @end
+%%=============================================================================
+
+-include_lib("deps/epgsql/include/pgsql.hrl").
+
+
+-record(state, {connection, graph, version, roles, namespaces, tables}).
+
+-record(pg_acl, {role, granted_by, select, usage, insert, update, delete, trigger, truncate, references, execute, create, connect, temporary}).
+-record(pg_namespace, {name, owner, acls, oid}).
+-record(pg_role, {name, super_user, inherit, create_role, create_db, catalog_update, can_login, replication, conn_limit, password, valid_until, config, oid}).
+-record(pg_table, {namespace, name, owner, acls, options, columns, constraints, oid}).
+
+
+-record(pg_extension, {name, schema, comment, owner}).
+-record(pg_column, {name, type, nullable}).
+-record(pg_domain, {name}).
+
+-record(pg_function, {name}).
+-record(pg_cast, {name}).
+-record(pg_rule, {name}).
+-record(pg_trigger, {name}).
+-record(pg_index, {name}).
+-record(pg_constraint, {name}).
