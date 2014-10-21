ef5a8b191c29fb801d3b52705c3dae04a1b834d6
Gavin M. Roy <gavinr@aweber.com>
2014-10-21 14:03:25 -0400

    Stub for DDL output

---
+++ src/pg_graph_ddl.erl
@@ -0,0 +1,12 @@
+%%%-------------------------------------------------------------------
+%%% @author Gavin M. Roy <gavinr@aweber.com>
+%%% @copyright (C) 2014, AWeber Communications
+%%% @doc Outputs the DDL for the PostgreSQL objects
+%%%
+%%% @end
+%%%-------------------------------------------------------------------
+-module(pg_graph_ddl).
+-author("gavinr").
+
+%% API
+-export([]).
